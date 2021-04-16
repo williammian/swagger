72db5ebdf8002376a99343ccf704ae97df04eb0c
William Mian <william_mian@yahoo.com.br>
2021-04-16 13:14:30 -0300

    exclusao de transacao

--- bytebank-api/src/main/java/io/swagger/api/TransacaoApiController.java
+++ bytebank-api/src/main/java/io/swagger/api/TransacaoApiController.java
@@ -1,11 +1,9 @@
 package io.swagger.api;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,18 +20,11 @@ import io.swagger.model.Transacao;
 @Controller
 public class TransacaoApiController implements TransacaoApi {
 
-private final HttpServletRequest request;
-    
 	@Autowired
 	private TransacaoService transacaoService;
 
 	@Autowired
 	private RespostasUtil respostasUtil;
-
-    @org.springframework.beans.factory.annotation.Autowired
-    public TransacaoApiController(HttpServletRequest request) {
-        this.request = request;
-    }
 
     public ResponseEntity<Void> alteraValorTransacao(@ApiParam(value = "",required=true) @PathVariable("codigo") Long codigo,@NotNull @ApiParam(value = "", required = true) @Valid @RequestParam(value = "valor", required = true) Double valor,@ApiParam(value = "" ,required=true) @RequestHeader(value="Authorization", required=true) String authorization) {
     	try {
@@ -52,8 +43,11 @@ private final HttpServletRequest request;
     }
 
     public ResponseEntity<Void> excluiTransacao(@ApiParam(value = "",required=true) @PathVariable("codigo") Long codigo,@ApiParam(value = "" ,required=true) @RequestHeader(value="Authorization", required=true) String authorization) {
-        String accept = request.getHeader("Accept");
-        return new ResponseEntity<Void>(HttpStatus.NOT_IMPLEMENTED);
+    	try {
+			return transacaoService.exclui(authorization, codigo);
+		} catch (Exception e) {
+			return respostasUtil.getErroInterno(RespostasUtil.MENSAGEM_FALHA_AO_TENTAR_EXCLUIR_TRANSACAO);
+		}
     }
 
 }
