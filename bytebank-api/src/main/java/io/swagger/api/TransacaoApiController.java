b4d0a6ac167fe3bb349863d1566ba7489b31b38d
William Mian <william_mian@yahoo.com.br>
2021-04-16 11:13:32 -0300

    consulta extrato e altera valor transacao

--- bytebank-api/src/main/java/io/swagger/api/TransacaoApiController.java
+++ bytebank-api/src/main/java/io/swagger/api/TransacaoApiController.java
@@ -36,8 +36,11 @@ private final HttpServletRequest request;
     }
 
     public ResponseEntity<Void> alteraValorTransacao(@ApiParam(value = "",required=true) @PathVariable("codigo") Long codigo,@NotNull @ApiParam(value = "", required = true) @Valid @RequestParam(value = "valor", required = true) Double valor,@ApiParam(value = "" ,required=true) @RequestHeader(value="Authorization", required=true) String authorization) {
-        String accept = request.getHeader("Accept");
-        return new ResponseEntity<Void>(HttpStatus.NOT_IMPLEMENTED);
+    	try {
+			return transacaoService.atualiza(authorization, codigo, valor);
+		} catch (Exception e) {
+			return respostasUtil.getErroInterno(RespostasUtil.MENSAGEM_FALHA_AO_TENTAR_ATUALIZAR_TRANSACAO);
+		}
     }
 
     public ResponseEntity<Transacao> cadastraTransacao(@ApiParam(value = "" ,required=true) @RequestHeader(value="Authorization", required=true) String authorization,@ApiParam(value = "" ,required=true )  @Valid @RequestBody Transacao transacao,@NotNull @ApiParam(value = "", required = true, allowableValues = "debito, deposito") @Valid @RequestParam(value = "tipo", required = true) String tipo) {
